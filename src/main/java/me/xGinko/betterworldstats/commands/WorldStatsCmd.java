965b334d87cb6976ec99992ded81f7f5ff36e9fb
xGinko <rachel.audrey@mail.de>
2023-01-14 18:16:48 +0100

    cleanup unused imports

--- src/main/java/me/xGinko/betterworldstats/commands/WorldStatsCmd.java
+++ src/main/java/me/xGinko/betterworldstats/commands/WorldStatsCmd.java
@@ -3,13 +3,10 @@ package me.xGinko.betterworldstats.commands;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.xGinko.betterworldstats.BetterWorldStats;
 import me.xGinko.betterworldstats.config.ConfigCache;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import java.util.Calendar;
