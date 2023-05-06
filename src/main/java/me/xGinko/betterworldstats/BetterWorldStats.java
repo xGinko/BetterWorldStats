3ea945d9260d282bd55f2f121bd6716195ca35cc
xGinko <rachel.audrey@mail.de>
2023-05-06 17:27:03 +0200

    remove imports

--- src/main/java/me/xGinko/betterworldstats/BetterWorldStats.java
+++ src/main/java/me/xGinko/betterworldstats/BetterWorldStats.java
@@ -15,8 +15,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
 
 import java.io.File;
 import java.io.IOException;
