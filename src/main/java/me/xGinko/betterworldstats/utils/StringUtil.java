7535520a985cb41cdc7e93c909ee1463e0126934
xGinko <rachel.audrey@mail.de>
2024-01-01 00:59:59 +0100

    remove redundant code

--- src/main/java/me/xGinko/betterworldstats/utils/StringUtil.java
+++ src/main/java/me/xGinko/betterworldstats/utils/StringUtil.java
@@ -1,10 +1,10 @@
 package me.xGinko.betterworldstats.utils;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import me.xGinko.betterworldstats.BetterWorldStats;
 
 public class StringUtil {
+
     public static String tryPopulateWithPAPI(String input) {
-        return BetterWorldStats.foundPlaceholderAPI ? PlaceholderAPI.setPlaceholders(null, input) : input;
+        return PlaceholderAPI.setPlaceholders(null, input);
     }
 }
