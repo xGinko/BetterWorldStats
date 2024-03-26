a2fd94faf9c3483f7941422098bba3c747e2aca0
xGinko <rachel.audrey@mail.de>
2024-03-26 13:55:01 +0100

    FileStats

--- src/main/java/me/xginko/betterworldstats/stats/FileStats.java
+++ src/main/java/me/xginko/betterworldstats/stats/FileStats.java
@@ -8,28 +8,32 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class FileStats {
 
     private final @NotNull Config config;
-    private final @NotNull AtomicReference<ScanResult> scan_result;
+    private final @NotNull AtomicReference<FileScanResult> scan_result;
+    private final @NotNull AtomicInteger chunk_count, entity_count;
     private boolean scanning = false;
 
     public FileStats() {
         this.config = BetterWorldStats.getConfiguration();
         this.scan_result = new AtomicReference<>();
+        this.chunk_count = new AtomicInteger();
+        this.entity_count = new AtomicInteger();
         this.refresh();
     }
 
     private void refresh() {
-        if (System.currentTimeMillis() <= scan_result.get().expiration_time_millis || scanning) {
+        if (scan_result.get() != null && (System.currentTimeMillis() <= scan_result.get().expiration_time_millis || scanning)) {
             return;
         }
 
         scanning = true;
 
-        CompletableFuture.supplyAsync(() -> new ScanResult(config.paths_to_scan, config.filesize_update_period_millis)).thenAccept(result -> {
+        CompletableFuture.supplyAsync(() -> new FileScanResult(config.paths_to_scan, config.filesize_update_period_millis)).thenAccept(result -> {
             scan_result.set(result);
             scanning = false;
             if (config.log_is_enabled) {
@@ -39,10 +43,15 @@ public class FileStats {
                         "Size: " + config.filesize_format.format(result.size_in_gb) + "GB, " +
                                 "files: " + result.file_count + ", " +
                                 "folders: " + result.folder_count + ", " +
-                                "chunks: " + result.chunk_count + ", " +
-                                "entities: " + result.entity_count).color(BetterWorldStats.COLOR));
+                                "chunks: " + chunk_count + ", " +
+                                "entities: " + entity_count).color(BetterWorldStats.COLOR));
             }
         });
+
+        for (final World world : BetterWorldStats.getInstance().getServer().getWorlds()) {
+            this.chunk_count.addAndGet(world.getChunkCount());
+            this.entity_count.addAndGet(world.getEntityCount());
+        }
     }
 
     public String getSize() {
@@ -67,33 +76,26 @@ public class FileStats {
 
     public String getChunkCount() {
         refresh();
-        return Integer.toString(scan_result.get().chunk_count);
+        return chunk_count.toString();
     }
 
     public String getEntityCount() {
         refresh();
-        return Integer.toString(scan_result.get().entity_count);
+        return entity_count.toString();
     }
 
-    private static class ScanResult {
+    private static class FileScanResult {
 
         public final long expiration_time_millis;
         public final double size_in_gb;
-        public int file_count, folder_count, chunk_count, entity_count;
+        public int file_count, folder_count;
 
-        protected ScanResult(@NotNull Iterable<String> paths_to_scan, long cooldown_millis) {
-            this.file_count = this.chunk_count = this.folder_count = 0;
-
+        protected FileScanResult(@NotNull Iterable<String> paths_to_scan, long cooldown_millis) {
+            this.file_count = this.folder_count = 0;
             long byteSize = 0L;
             for (String path : paths_to_scan)
                 byteSize += this.getByteSize(new File(path));
             this.size_in_gb = byteSize / 1048576.0D / 1000.0D;
-
-            for (final World world : BetterWorldStats.getInstance().getServer().getWorlds()) {
-                this.chunk_count += world.getChunkCount();
-                this.entity_count += world.getEntityCount();
-            }
-
             this.expiration_time_millis = System.currentTimeMillis() + cooldown_millis;
         }
 
